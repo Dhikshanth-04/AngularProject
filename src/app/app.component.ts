1ffad1258f6f217dad3f26e45045efee6f5e2392
Dhikshanth-04 <squadattack9@gmail.com>
2026-07-05 11:45:16 +0530

    Learning notes are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-angular';
+  ang=true
+  name="angular"
+  
+  
+  fNameLabel = "Enter First name : "
+  fNameInput = "SiiRii"
+  
+  emailLabel = "Enter Email ID : "
+  emailInput = "siirii@gmail.com"
+  
+  a(){
+    alert(`the user name : ${this.fNameInput} \nthe email id : ${this.emailInput}`)
+  }
+
+
+count:number = 0;
+
+add(){
+  this.count++;
+}
+
+sub(){
+  this.count--;
+}
+reset(){
+  this.count=0;
+}
+
+string:string = "";
+get length(): number{
+  return this.string.length;
+}
 }
